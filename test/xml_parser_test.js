8facca95b0040b4dbee66ad4477ef0a6c2f2fb7c
James E. Marca <jmarca@translab.its.uci.edu>
2014-02-18 18:14:39 -0800

    test is passing.
    dumping lots of debug for edification

--- test/xml_parser_test.js
+++ test/xml_parser_test.js
@@ -5,21 +5,16 @@ var parser_maker = require('../lib/xml_parser.js')
 var request = require('request')
 
 var env = process.env;
-var chost = env.CAS_HOST;
-var cuser = env.CAS_USER;
-var cpass = env.CAS_PASS;
-var casservice = 'https://'+chost+'/'
-var casurl = casservice + 'cas/login'
 
 var testhost = env.CAS_VALIDATE_TEST_URL || '127.0.0.1'
 var testport = env.CAS_VALIDATE_TEST_PORT || 3000
 
 testport += 2
 
+var express = require('express')
 before(function(done){
     // a small server to run while testing the UI
 
-    var express = require('express')
 
     var app = express().use(express.logger())
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }))
@@ -30,44 +25,94 @@ before(function(done){
     return null
 })
 
-// after kill the server?
+var RedisStore = require('connect-redis')(express);
 
 describe('get a user doc, parse the umlaut',function(){
     it('should parse the umlaut just fine'
       ,function(done){
-           var _req = {'session':{'attributes':{}
-                                 ,'ticket':'blah blah'
-                                 }
-                      }
-           var _res = {} // don't need for res to do anything in this test
-           var parser = parser_maker.make_xml_parser(_req,_res,function(e){
-                            console.log(_req)
-                            _req.session.name.should.eql('h_mueller')
-                            var attrs = _req.session.attributes
-                            attrs.should.have.keys(['mail'
-                                                   ,'__AUTHUSERCONTEXT__'
-                                                   ,'cn'
-                                                   ,'__AUTHTYPE__'
-                                                   ,'surname'
-                                                   ,'tudUserUniqueID'
-                                                   ,'givenName'
-                                                   ])
-                            attrs.mail.should.equal('a.b@c.de')
+           var port = testport+1
+           var srvr = express().use(express.logger())
+           srvr.use(express.errorHandler({ dumpExceptions: true, showStack: true }))
+           .use(express.cookieParser('barley wheat napoleon'))
+           .use(express.session({ store: new RedisStore }))
+           srvr.get('/test', function(req, res){
+               // short circuit a real request cycle
+               req.session.ticket='testingticket'
+
+               var parser = parser_maker.make_xml_parser(req,res,function(e){
+                                req.session.name.should.eql('h_mueller')
+                                var attrs = req.session.attributes
+                                attrs.should.have.keys(['mail'
+                                                       ,'__AUTHUSERCONTEXT__'
+                                                       ,'cn'
+                                                       ,'__AUTHTYPE__'
+                                                       ,'surname'
+                                                       ,'tudUserUniqueID'
+                                                       ,'givenName'
+                                                       ])
+                                attrs.mail.should.equal('a.b@c.de')
 
 
-                            attrs[ '__AUTHUSERCONTEXT__'].should.equal( 'cont' )
-                            attrs[ 'cn'].should.equal( 'commonname' )
-                            attrs[ '__AUTHTYPE__'].should.equal( 'TUID' )
-                            attrs[ 'surname'].should.equal( 'Müller' )
-                            attrs[ 'tudUserUniqueID'].should.equal( '1234567' )
-                            attrs[ 'givenName'].should.equal( 'Hans' )
-                            return done()
-                        })
-           request.get('http://'+testhost+':'+testport+'/cas_auth.xml',function(e,r,b){
-               //console.log(r)
-               should.not.exist(e)
-               parser(b)
-               return  null
+                                attrs[ '__AUTHUSERCONTEXT__'].should.equal( 'cont' )
+                                attrs[ 'cn'].should.equal( 'commonname' )
+                                attrs[ '__AUTHTYPE__'].should.equal( 'TUID' )
+                                attrs[ 'surname'].should.equal( 'Müller' )
+                                attrs[ 'tudUserUniqueID'].should.equal( '1234567' )
+                                attrs[ 'givenName'].should.equal( 'Hans' )
+                                return done()
+                            })
+               request.get('http://'+testhost+':'+testport+'/cas_auth.xml'
+                          ,function(e,r,b){
+                               should.not.exist(e)
+                               parser(b)
+                               return  null
+                           })
+
+           });
+           srvr.listen(port,testhost,function(){
+               var j = request.jar()
+               request({'uri':'http://'+testhost+':'+port+'/test'
+                       ,'jar':j}
+                      ,function(e,r,b){
+                           return null
+                       })
+
+           })
+           return null
+       })
+})
+describe('handle a failed session login',function(){
+    it('should parse fail xml response just fine'
+      ,function(done){
+           var port = testport+2
+           var srvr = express().use(express.logger())
+           srvr.use(express.errorHandler({ dumpExceptions: true, showStack: true }))
+           .use(express.cookieParser('barley wheat napoleon'))
+           .use(express.session({ store: new RedisStore }))
+           srvr.get('/test', function(req, res){
+               // short circuit a real request cycle
+               req.session.ticket='testingticket'
+
+               var parser = parser_maker.make_xml_parser(req,res,function(e){
+                            })
+               request.get('http://'+testhost+':'+testport+'/cas_fail.xml'
+                          ,function(e,r,b){
+                               should.not.exist(e)
+                               parser(b)
+                               return  null
+                           })
+
+           });
+           srvr.listen(port,testhost,function(){
+               var j = request.jar()
+               request({'uri':'http://'+testhost+':'+port+'/test'
+                       ,'jar':j
+                       ,'followRedirect':false}
+                      ,function(e,r,b){
+                           // should have received a 307
+                           r.statusCode.should.equal(307)
+                           return done()
+                       })
            })
            return null
        })
