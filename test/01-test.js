4fe36ccbdaf4bf200b06201131f2cfb6aba90ebe
James E. Marca <james@activimetrics.com>
2019-07-24 17:38:55 -0700

    basic basic tests work
    had trouble with closing tests because the redis client in the
    ticket_store was living forever, with no hook to close it.  For now I
    just tucked both instances inside of the ticket set and ticket get
    actions

--- test/01-test.js
+++ test/01-test.js
@@ -10,11 +10,12 @@ const casservice = 'https://'+chost+':'+cport+'/'
 const casurl = casservice + 'cas/login'
 
 const testhost = env.CAS_VALIDATE_TEST_URL || '127.0.0.1'
-const testport = env.CAS_VALIDATE_TEST_PORT || 3000
+var testport = env.CAS_VALIDATE_TEST_PORT || 3000
 
 const sa = require('superagent')
 const agent = sa.agent();
 const redishost = env.REDIS_HOST || 'redis'
+const redis = require('redis')
 
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
@@ -24,78 +25,151 @@ process.env.CAS_SESSION_TTL=2
 const cas_validate = require('../lib/cas_validate')
 const http = require('http')
 
-var server
-function setup_server(t){
+
+var sockets=[]
+
+var client
+
+function setup_server(){
+    const port = testport++
+    const store = new RedisStore({host:redishost,  ttl: 100})
     const app = connect()
-          .use(session({ store: new RedisStore({host:redishost,
-                                               }),
-                         secret: 'barley waterloo napoleon',
-                         resave: false,
-                         saveUninitialized: true,
+          .use(session({ 'store': store,
+                         'secret': 'barley waterloo napoleon',
+                         'resave': false,
+                         'saveUninitialized': true,
 
                        }))
           .use(cas_validate.redirect({'cas_host':chost
-                                      ,'service':'http://'+ testhost +':'+testport+'/index.html'})
+                                      ,'service':'http://'+ testhost +':'+port+'/index.html'})
               )
           .use(function(req, res, next){
               res.statusCode = 404
               res.end('bad news kid\n')
               return null
           })
-    server = http.createServer(app)
-    t.context.server = server
-    server.listen(testport, testhost, function(){
-        // standard node-style callback
-        // if you call this with an error, it'll blow up
-        console.log('listening on ',testhost,testport)
-        t.pass('relax')
-        t.end()
+    return new Promise((resolve,reject)=>{
+        const server = http.createServer(app)
+        server.listen(port, testhost, function(){
+            console.log('listening on ',testhost,port)
+            resolve({'server':server,
+                     'store':store,
+                     'port':port})
+        })
+    })
+
+}
+function setup_server_http(){
+    const port = testport++
+    const store = new RedisStore({host:redishost,  ttl: 100})
+    const app = connect()
+          .use(session({ 'store': store,
+                         'secret': 'barley waterloo napoleon',
+                         'resave': false,
+                         'saveUninitialized': true,
+
+                       }))
+          .use(cas_validate.redirect({'cas_host':'http://'+chost
+                                      ,'service':'http://'+ testhost +':'+port+'/index.html'})
+              )
+          .use(function(req, res, next){
+              res.statusCode = 404
+              res.end('bad news kid\n')
+              return null
+          })
+    return new Promise((resolve,reject)=>{
+        const server = http.createServer(app)
+        server.listen(port, testhost, function(){
+            console.log('listening on ',testhost,port)
+            resolve({'server':server,
+                     'store':store,
+                     'port':port})
+        })
+    })
+}
+function setup_server_https(){
+    const port = testport++
+    const store = new RedisStore({host:redishost,  ttl: 100})
+    const app = connect()
+          .use(session({ 'store': store,
+                         'secret': 'barley waterloo napoleon',
+                         'resave': false,
+                         'saveUninitialized': true,
+
+                       }))
+          .use(cas_validate.redirect({'cas_host':'https://'+chost
+                                      ,'service':'http://'+ testhost +':'+port+'/index.html'})
+              )
+          .use(function(req, res, next){
+              res.statusCode = 404
+              res.end('bad news kid\n')
+              return null
+          })
+    return new Promise((resolve,reject)=>{
+        const server = http.createServer(app)
+        server.listen(port, testhost, function(){
+            console.log('listening on ',testhost,port)
+            resolve({'server':server,
+                     'store':store,
+                     'port':port})
+        })
     })
 }
 
-tap.test('server test',async (t)=>{
 
-
-    tt = await t.test('setup server',setup_server)
-    console.log('server is?', server)
-
-    await t.test('should redirect when no session is established', tt => {
-        agent
-            .get('http://'+ testhost +':'+testport+'/')
-            .ok(res => res.status < 400)
-            .redirects(0)
-
-            .then( res => {
-                console.log('back from server with ',res.status)
-                tt.is(res.status,307)
-                tt.is(res.headers.location,casurl+'?service=http%3A%2F%2F'+ testhost +'%3A'+testport+'%2Findex.html')
-                console.log('did it work?')
-            }).catch(e=>{
-                console.log('agent get threw up')
-                console.log(e)
-            }).then(()=>{
-                console.log('calling tt.end')
-                tt.end()
-            })
-    })
-
-    console.log('hate coursing through your veins')
-
-    t.test(async (tt) =>{
-        console.log('closing server')
-        await   new Promise(resolve => {
-            server.close( (e,r)=>{
-                console.log('server closed')
-                console.log('e is',e)
-                console.log('r is',r)
-                resolve('resolved');
-                tt.pass()
-            })
+function close_server(server_store){
+    console.log('closing server')
+    const result = new Promise(resolve => {
+        server_store.store.client.quit()
+        server_store.server.close( (e,r)=>{
+            console.log('server closed')
+            return resolve()
         })
-    }).then( ()=>{
-        console.log('are we done with teardown?')
-        t.end()
-
     })
+    return result
+}
 
-})
+function server_test(setup){
+
+    async function handler (t){
+        const server_store = await setup()
+        const myport = server_store.port
+        const result = await agent
+              .get('http://'+ testhost +':'+myport+'/')
+              .ok(res => res.status < 400)
+              .redirects(0)
+
+              .then( res => {
+                  console.log('back from server with ',res.status)
+                  t.is(res.status,307)
+                  t.is(res.headers.location,casurl+'?service=http%3A%2F%2F'+ testhost +'%3A'+myport+'%2Findex.html')
+              }).catch(e=>{
+                  console.log('agent get threw up')
+                  console.log(e)
+                  t.fail(e)
+              }).then(()=>{
+                  console.log('calling t.end')
+                  t.end()
+              }).then(()=>{
+                  return close_server(server_store)
+              })
+        console.log('hate coursing through your veins')
+    }
+    return handler
+}
+
+
+function runit() {
+    return tap.test('initialize server',server_test(setup_server))
+        .then(()=>{
+            tap.test('initialize server http',server_test(setup_server_http))
+        })
+        .then(()=>{
+            tap.test('initialize server https',server_test(setup_server_https))
+        })
+        .then(()=>{
+            tap.end()
+        })
+}
+
+runit()
