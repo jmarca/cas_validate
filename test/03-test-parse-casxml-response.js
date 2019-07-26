cabfebfcf0da7cb8f28cd0fbf9c3eadbe9726ea0
James E. Marca <james@activimetrics.com>
2019-07-26 15:59:46 -0700

    getting there, but not there yet
    need to set up cas user, cas password in test docker server

--- test/03-test-parse-casxml-response.js
+++ test/03-test-parse-casxml-response.js
@@ -11,7 +11,6 @@ const casurl = casservice + 'cas/login'
 
 const testhost = env.CAS_VALIDATE_TEST_URL || 'cas_node_tests'
 var testport = env.CAS_VALIDATE_TEST_PORT || 3000
-const server_store = {}
 
 const request = require('request');
 const agentOptions = {
@@ -36,49 +35,60 @@ const http = require('http')
 // need to set up a server running bits and pieces of sas validate to test this properly.
 // because the tests are responding to incoming connections.
 
-function _login_handler(rq,callback){
-    return function(e,r,b){
-        // parse the body for the form url, with the correct jsessionid
-        var form_regex = /id="fm1".*action="(.*)" method="post"/;
-        var result = form_regex.exec(b)
-        var opts={}
-        opts.url=casservice+result[1]
-        opts.form={'username':cuser
-                  ,'password':cpass
-                  ,'submit':'LOGIN'
-                  }
-        opts.followRedirect=true
-        // scrape hidden input values
-        var name_regex = /name="(.*?)"/
-            var value_regex = /value="(.*?)"/
-            var hidden_regex = /<input.*type="hidden".*?>/g
-        while ((result = hidden_regex.exec(b)) !== null)
-        {
-            var n = name_regex.exec(result[0])
-            var v = value_regex.exec(result[0])
-            opts.form[n[1]]=v[1]
-        }
-        // console.log('opts is' )
-        // console.log(opts)
-        // console.log('--' )
-        rq.post(opts,callback)
+function _login_handler(b){
+    // parse the body for the form url, with the correct jsessionid
+    var form_regex = /id="fm1".*action="(.*)">/;
+    var result = form_regex.exec(b)
+    console.log(result[0],result[1])
+    var opts={}
+    opts.url=casservice+result[1]
+    opts.form={'username':cuser
+               ,'password':cpass
+               ,'submit':'LOGIN'
+              }
+    // scrape hidden input values
+    var name_regex = /name="(.*?)"/
+    var value_regex = /value="(.*?)"/
+    var hidden_regex = /<input.*type="hidden".*?>/g
+    while ((result = hidden_regex.exec(b)) !== null)
+    {
+        var n = name_regex.exec(result[0])
+        var v = value_regex.exec(result[0])
+        opts.form[n[1]]=v[1]
     }
+    console.log('opts is' )
+    console.log(opts)
+    console.log('--' )
+    return opts
 }
 
-function cas_login_function(rq,callback){
-    var opts ={url:casurl}
 
-             rq(opts
-               ,_login_handler(rq
-                              ,function(e,r,b){
-                                   var success_regex = /Log In Successful/i;
-                                   if(success_regex.test(b)){
-                                       return callback()
-                                   }else{
-                                       return callback('CAS login failed')
-                                   }
-                               }) )
-         }
+function cas_login_function(j){
+    var opts ={'url':casurl
+               , 'jar': j
+               , 'agentOptions': {
+                   'rejectUnauthorized': false
+               }
+               ,"followRedirect":true
+              }
+    const result = new Promise((resolve,reject)=>{
+        request(opts
+           ,(e,r,b)=>{
+               console.log('log in to cas, response is:',b)
+               Object.assign(opts,_login_handler(b))
+               request(opts
+                  ,(ee,rr,bb)=>{
+                      var success_regex = /Log In Successful/i;
+                      console.log(bb)
+                      if(success_regex.test(bb)){
+                          return resolve()
+                      }else{
+                          return reject('CAS login failed')
+                      }
+                  })
+           })
+    })
+}
 
 function cas_logout_function(rq,callback){
     var logouturl = 'https://'+chost + '/cas/logout';
@@ -92,7 +102,7 @@ function cas_logout_function(rq,callback){
 }
 
 function setup_server(){
-    const port = testport++
+    const port = testport
     const store = new RedisStore({host:redishost,  ttl: 100})
     const app = connect()
           .use(session({ 'store': store,
@@ -135,7 +145,7 @@ function setup_server(){
     return new Promise((resolve,reject)=>{
         const server = http.createServer(app)
         server.listen(port, testhost, function(){
-            console.log('server up and running',testhost,port)
+            console.log('server up:',testhost,port)
             resolve({'server':server,
                      'store':store,
                      'port':port})
@@ -146,9 +156,11 @@ function setup_server(){
 
 
 function close_server(server_store){
+    console.log('closing server')
     const result = new Promise(resolve => {
         server_store.store.client.quit()
         server_store.server.close( (e,r)=>{
+            console.log('server closed')
             return resolve()
         })
     })
@@ -156,51 +168,18 @@ function close_server(server_store){
 }
 
 async function no_session(t){
+    const server_store = t.context.server_store
+    const myport = server_store.port
     const j = request.jar()
 
-    const myport = server_store.port
-    console.log('http://'+ testhost + ':' + myport + '/attributes')
+
     const result = new Promise((resolve,reject) => {
         request({'url': 'http://'+ testhost + ':' + myport + '/attributes'
                  , 'jar': j
                  ,agentOptions: {
                      rejectUnauthorized: false
-                 }}
-                , (e,r,b) => {
-                    try {
-                        t.notOk(e)
-                        console.log('e is ',e)
-                        //console.log('r is ',r)
-                        console.log('b is ',b)
-                        t.equal(r.statusCode,200)
-                        t.ok(b)
-                        t.same(JSON.parse(b),{})
-                    }catch(e){
-                        console.log(e)
-                        t.fail()
-                        return reject(e)
-                    }
-                    return resolve()
-                });
-    })
-    await result
-        .catch( e =>{
-            console.log(e)
-        })
-    t.end()
-    return close_server(server_store)
-}
-
-async function get_user_name(t){
-    const j = request.jar()
-
-    const myport = server_store.port
-    const result = new Promise((resolve,reject) => {
-        request({'url': 'http://'+ testhost + ':' + myport + '/attributes'
-                 , 'jar': j
-                 ,agentOptions: {
-                     rejectUnauthorized: false
-                 }}
+                 }
+                 ,followRedirect:true}
                 , (e,r,b) => {
                     try {
                         t.notOk(e)
@@ -222,20 +201,64 @@ async function get_user_name(t){
         .catch( e =>{
             console.log(e)
         })
-    return t.end()
+    t.end()
 
 }
 
-async function runit(){
-    const _server_store = await setup_server()
-    server_store.server = _server_store.server
-    server_store.store = _server_store.store
-    server_store.port = _server_store.port
+async function user_name_session(t){
+    const server_store = t.context.server_store
+    const myport = server_store.port
+    const j = request.jar()
 
-    tap.test('should reply with an empty json object when no session is established',no_session)
+    const result = new Promise((resolve,reject) => {
+        // set up a session with CAS server
+        cas_login_function(j
+                           ,function(e){
+                                                    return cb(e,rq)
+                                                })
 
-    tap.end()
-    await close_server(server_store)
+        request({'url': 'http://'+ testhost + ':' + myport + '/attributes'
+                 , 'jar': j
+                 ,agentOptions: {
+                     rejectUnauthorized: false
+                 }
+                 ,followRedirect:true}
+                , (e,r,b) => {
+                    try {
+                        t.notOk(e)
+                        //console.log('e is ',e)
+                        //console.log('r is ',r)
+                        console.log('b is ',b)
+                        t.equal(r.statusCode,200)
+                        t.ok(b)
+                        t.same(JSON.parse(b),{})
+                    }catch(e){
+                        console.log(e)
+                        t.fail()
+                        return reject(e)
+                    }
+                    return resolve()
+                });
+    })
+    await result
+        .catch( e =>{
+            console.log(e)
+        })
+    t.end()
+
 }
 
-runit()
+
+setup_server()
+    .then(server_store =>{
+        tap.context.server_store = server_store
+        tap.test('should reply with an empty json object when no session is established',no_session)
+            .then(async ()=>{
+                await tap.test('should return the current user name when there is a session',user_name_session)
+
+            })
+            .then(()=>{
+                tap.end()
+                return close_server(server_store)
+            })
+    })
